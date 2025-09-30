3311fa7a8185ac16d8abc038a1a810e541b58dc2
Zoubird <80945377+Zoubird@users.noreply.github.com>
2025-09-30 08:49:33 +0200

    Update main.py
    Changed room_schedule

--- main.py
+++ main.py
@@ -2,18 +2,53 @@ class Scheduler():
     def __init__(self):
         # Available classrooms (these are not all the rooms, more will be included later)
         self.av_rooms = [002, 009, 010, 011, 012, 106, 108, 112, 113, 114, 115, 116, 201, 202, 203, 204, 205]
-        # Teacher (more will be added later)
-        self.teachers = ["Teacher1", "Teacher2", "Teacher3", "Teacher4", "Teacher5", "Teacher6"]
         self.room_schedule = {
             "Mon": {
-                1: [{"room": 002, "class": "3A"}, {"room": 112, "class": "2B"}],
+                1: [Exam(room=108, group="3VA"), Exam(room=116, group="2HB")],
                 2: [{"room": 009, "class": "2A"}, {"room": 113, "class": "1B"}],
                 3: [],
                 4: [],
                 5: [],
                 6: [],
-                7: []}}
-        self.teacher_schedule = {}
+                7: []
+            },
+            "Tu": {
+                1: [Exam(room=108, group="3VA"), Exam(room=116, group="2HB")],
+                2: [{"room": 009, "class": "2A"}, {"room": 113, "class": "1B"}],
+                3: [],
+                4: [],
+                5: [],
+                6: [],
+                7: []
+            },
+            "Wed": {
+                1: [Exam(room=108, group="3VA"), Exam(room=116, group="2HB")],
+                2: [{"room": 009, "class": "2A"}, {"room": 113, "class": "1B"}],
+                3: [],
+                4: [],
+                5: [],
+                6: [],
+                7: []
+            },
+            "Thu": {
+                1: [Exam(room=108, group="3VA"), Exam(room=116, group="2HB")],
+                2: [{"room": 009, "class": "2A"}, {"room": 113, "class": "1B"}],
+                3: [],
+                4: [],
+                5: [],
+                6: [],
+                7: []
+            },
+            "Fri": {
+                1: [Exam(room=108, group="3VA"), Exam(room=116, group="2HB")],
+                2: [{"room": 009, "class": "2A"}, {"room": 113, "class": "1B"}],
+                3: [],
+                4: [],
+                5: [],
+                6: [],
+                7: []
+            }
+        }
 
     def _ext_data_form(self):
         # Later, this function will serve purpose to extract data from the data that has been provided by someone using the algo
