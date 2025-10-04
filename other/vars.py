a0961931e4ec0429ba4298cc638ff696ca38849e
Zoubird <80945377+Zoubird@users.noreply.github.com>
2025-10-04 19:00:57 +0200

    Add files via upload

---
+++ other/vars.py
@@ -0,0 +1,5 @@
+classes = {
+    1: [{"H": 4, "V": 3}, ["netl", "entl", "fatl", "sptl", "ges", "wis"]],
+    2: [{"H": 3, "V": 3}, ["netl", "entl", "fatl", "dutl", "sptl", "ges", "nask", "wis"]],
+    3: [{"H": 3, "V": 2}, ["netl", "entl", "fatl", "sptl", "eco", "ges", "nat", "sch", "wis"]]
+}
