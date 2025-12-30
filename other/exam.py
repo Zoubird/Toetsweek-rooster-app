88f5300cadc6e348d0a7673fb1e6cd842e8f3345
Zoubird <80945377+Zoubird@users.noreply.github.com>
2025-12-30 18:04:53 +0100

    Add Exam class with initialization parameters

---
+++ other/exam.py
@@ -0,0 +1,9 @@
+class Exam():
+    def __init__(self,group_id,day,period,subject,duration,room,supervisor):
+        self.group_id = group_id
+        self.day = day
+        self.period = period
+        self.subject = subject
+        self.duration = duration
+        self.room = room
+        self.supervisor = supervisor
